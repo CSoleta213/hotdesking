a6dcf6950b107cc1e55fea4482efad9333e86d26
Andrea Maurice <andreamauricedg@gmail.com>
2022-03-21 22:32:36 +0800

    added brand

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,43 @@
 @extends('layouts.layout')
 
 @section('content')
-    <div class="navbar">
-      <a href="/">Logo</a>
-      <a href="/">Home</a>
-      <a href="/features">Features</a>
-      <a href="/faqs">FAQs</a>
-      <a href="/demo">Demo</a>
-      @if(Session::get('booker'))
-      <a href="http://">Welcome, {{Session::get('booker')}}</a>
-      @else
-      <a href="/log-in">Log in</a>
-      <a href="/sign-up" >Sign up</a>
-      @endif
-    </div>
+<div class="navbar">
+  <a href="/">Logo</a>
+  <a href="/">Home</a>
+  <a href="/features">Features</a>
+  <a href="/faqs">FAQs</a>
+  <a href="/demo">Demo</a>
+  @if(Session::get('booker'))
+  <a href="http://">Welcome, {{Session::get('booker')}}</a>
+  @else
+  <a href="/log-in">Log in</a>
+  <a href="/sign-up">Sign up</a>
+  @endif
+</div>
 
-    <div class="main">
-      <h1>Homepage</h1>
+<div class="home">
+  <!-- BRAND -->
+  <div class="brand">
+    <div class="tagContainer">
+      <div class="title">
+        <h1>The desk that is <span class="span-destined"> destined </span>to work with you</h1>
+      </div>
+      <div class="subtitle">
+        <h3>Desktiny will help you successfully transform the workspace from a place of work into a space that works.</h3>
+      </div>
+      <div class="button-reserve">
+        <a href="/log-in">Reserve a seat</a>
+      </div>
     </div>
+    <div class="pictureContainer">
+      <div class="blob"></div>
+      <img src="{{ asset('/perspective_matte.png') }}" class="img-desk">
+    </div>
+  </div>
+
+  <!-- COUNT -->
+  
+
+</div>
+
 @endsection
