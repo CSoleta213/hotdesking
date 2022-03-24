94ed912c016eee94a30b2c364dd91b859bf53958
Jsvrns <72838950+JesaViernes033@users.noreply.github.com>
2022-03-24 23:03:04 +0800

    features page minor change

--- resources/views/features.blade.php
+++ resources/views/features.blade.php
@@ -39,8 +39,8 @@
         <div class="section-container">
           <div class="columns image" style="background: #F2A645"></div>
           
-          <div class="columns content">
-              <div class="content-container">
+          <div class="columns content" >
+              <div class="content-container" style="margin-left:50px;">
               <h5 style="color: #F2A645">Calendar</h5>
                 <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -66,6 +66,22 @@
           </div>
           <div class="columns image" style="background: #06BB87;"> </div>
         </div>
+
+        <div class="section-container">
+          <div class="columns image" style="background: #F26849"></div>
+          
+          <div class="columns content">
+              <div class="content-container" style="margin-left:50px;">
+              <h5 style="color: #F26849">Calendar</h5>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                  in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                </p>      
+              </div>
+          </div>
+      </div>
        
 
     </div>
