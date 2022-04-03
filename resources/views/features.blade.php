22027e1db69a93c49dd46b5894ebf1aae208968e
Jsvrns <72838950+JesaViernes033@users.noreply.github.com>
2022-04-03 15:48:50 +0800

    features style

--- resources/views/features.blade.php
+++ resources/views/features.blade.php
@@ -27,8 +27,8 @@
     
       <div class="section-container">
           <div class="columns content">
-              <div class="content-container" style="margin-right: -50px;">
-                <h5 style="color: #FF4269">Calendar</h5>
+              <div class="content-container calendar" style="margin-right: -50px;">
+                <h5>Calendar</h5>
                 <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
@@ -37,15 +37,15 @@
                 </p>        
               </div>
           </div>
-          <div class="columns image" style="background: #FF4269"> </div>
+          <div class="columns image" id="calendar"> </div>
        </div>
 
         <div class="section-container">
-          <div class="columns image" style="background: #F2A645"></div>
+          <div class="columns image" id="office-view"></div>
           
           <div class="columns content" >
-              <div class="content-container" style="margin-right:-70px;">
-              <h5 style="color: #F2A645">Office View</h5>
+              <div class="content-container office-view" style="margin-right:-70px;">
+              <h5>Office View</h5>
                 <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
@@ -68,7 +68,7 @@
                 </p>        
               </div>
           </div>
-          <div class="columns image" style="background: #06BB87;"> </div>
+          <div class="columns image" id="desk-view"> </div>
         </div>
 
         <div class="section-container">
