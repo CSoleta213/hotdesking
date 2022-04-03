0aba3f19cdb84e501755168776dd5e63b7ffcbae
Carlo Soleta <carlosoleta@student.laverdad.edu.ph>
2022-04-03 16:04:13 +0800

    Prepared blank files for differect views

---
+++ resources/views/features/desk_map.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.layout')
+
+@section('content')
+
+<div class="home">
+  <h1>Book a desk</h1>
+  <a href="{{ url('/features/desk-map/book-a-desk') }}">1</a>
+</div>
+
+@endsection
