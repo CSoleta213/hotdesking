92d6734d8f33f89c50021d564909f4b1344d2170
Carlo Soleta <carlosoleta@student.laverdad.edu.ph>
2022-04-15 23:22:19 +0800

    List of Users Page

--- resources/views/admin/users_list.blade.php
+++ resources/views/admin/users_list.blade.php
@@ -6,31 +6,29 @@
       <span class="text">LIST OF USERS</span>
     </div>
     <div class="body-content">
-      <div class="home">
-        <table>
-          <tr>
-            <th>No.</th>
-            <th>Name of Employee</th>
-            <th>Phone Number</th>
-            <th>ID No.</th>
-            <th>Email</th>
-          </tr>
-          @foreach ($users as $user)
-          <tr>
-            <td>{{ $user->id }}</td>
-            <td>{{ $user->firstname }} {{ $user->lastname }}</td>
-            <td>{{ $user->phone_number }}</td>
-            <td>{{ $user->employee_id_number }}</td>
-            <td>{{ $user->email }}</td>
-          </tr>
-          @endforeach
-        </table>
-        @if (session('status'))
-          <div>
-            {{ session('status') }}
-          </div>
-        @endif
-      </div>
+      <table>
+        <tr>
+          <th>No.</th>
+          <th>Name of Employee</th>
+          <th>Phone Number</th>
+          <th>ID No.</th>
+          <th>Email</th>
+        </tr>
+        @foreach ($users as $user)
+        <tr>
+          <td>{{ $user->id }}</td>
+          <td>{{ $user->firstname }} {{ $user->lastname }}</td>
+          <td>{{ $user->phone_number }}</td>
+          <td>{{ $user->employee_id_number }}</td>
+          <td>{{ $user->email }}</td>
+        </tr>
+        @endforeach
+      </table>
+      @if (session('status'))
+        <div>
+          {{ session('status') }}
+        </div>
+      @endif
     </div>
   </section>
 @endsection
