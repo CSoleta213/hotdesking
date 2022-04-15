ae9618a90e4bfcb50c6de2d302c1b4eee0a2e993
Carlo Soleta <carlosoleta@student.laverdad.edu.ph>
2022-04-15 15:52:06 +0800

    Labeled five seats.

--- resources/views/features/desk_map.blade.php
+++ resources/views/features/desk_map.blade.php
@@ -11,11 +11,11 @@
         <img src="{{ asset('/desk-view.jpg') }}" alt="Desk View" width="100%">
         <div class="dots"></div>
           <!-- Trigger/Open The Modal -->
-          <button id="dot1" class="modal-button" href="#myModal1"></button>
-          <button id="dot2" class="modal-button" href="#myModal1"></button>
-          <button id="dot3" class="modal-button" href="#myModal1"></button>
-          <button id="dot4" class="modal-button" href="#myModal1"></button>
-          <button id="dot5" class="modal-button" href="#myModal1"></button>
+          <button id="dot1" class="modal-button" href="#myModal1">A1</button>
+          <button id="dot2" class="modal-button" href="#myModal1">A2</button>
+          <button id="dot3" class="modal-button" href="#myModal1">A3</button>
+          <button id="dot4" class="modal-button" href="#myModal1">A4</button>
+          <button id="dot5" class="modal-button" href="#myModal1">A5</button>
         </div>
       </div>
     </div>
@@ -30,19 +30,39 @@
       <form action="{{ url('/book-a-desk') }}" method="POST">
       @csrf
         <div class="modal-body">
-          <label for="name">Your Name</label>
+          <label for="name">Your Name:</label>
           <input type="text" id="name" name="name" value="{{ Auth::user()->firstname }} {{ Auth::user()->lastname }}" readonly><br><br>
 
-          <label for="office_name">Select an Office:</label>
+          <label for="office_name">Your Office:</label>
           <input type="text" id="office_name" name="office_name" value="{{ Auth::user()->office }}" readonly><br><br>
 
-          <label for="desk_number">Select Desk:</label>
+          <label for="desk_number">Your Chosen Desk:</label>
           <select name="desk_number" id="desk_number">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
+            <option value="A1">A1</option>
+            <option value="A2">A2</option>
+            <option value="A3">A3</option>
+            <option value="A4">A4</option>
+            <option value="A5">A5</option>
+            <option value="B1">B1</option>
+            <option value="B2">B2</option>
+            <option value="B3">B3</option>
+            <option value="B4">B4</option>
+            <option value="B5">B5</option>
+            <option value="C1">C1</option>
+            <option value="C2">C2</option>
+            <option value="C3">C3</option>
+            <option value="C4">C4</option>
+            <option value="C5">C5</option>
+            <option value="D1">D1</option>
+            <option value="D2">D2</option>
+            <option value="D3">D3</option>
+            <option value="D4">D4</option>
+            <option value="D5">D5</option>
+            <option value="E1">E1</option>
+            <option value="E2">E2</option>
+            <option value="E3">E3</option>
+            <option value="E4">E4</option>
+            <option value="E5">E5</option>
           </select><br><br>
 
           <label for="date">When?</label>
