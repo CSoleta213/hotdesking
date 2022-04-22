963713472ebba2743c9f092076e16d9d6a1e49c5
Carlo Soleta <carlosoleta@student.laverdad.edu.ph>
2022-04-23 00:20:16 +0800

    Welcome page responsiveness.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,7 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show-down");
 }
 
 // Close the dropdown if the user clicks outside of it
@@ -45,8 +45,8 @@ window.onclick = function(event) {
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+      if (openDropdown.classList.contains('show-down')) {
+        openDropdown.classList.remove('show-down');
       }
     }
   }
