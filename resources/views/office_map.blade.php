98c7987809cacebce2eb7335fc0e38b44d5547d1
Carlo Soleta <carlosoleta@student.laverdad.edu.ph>
2022-04-23 16:14:26 +0800

    Importing new design of office map.

--- resources/views/office_map.blade.php
+++ resources/views/office_map.blade.php
@@ -8,7 +8,7 @@
     <div class="body-content">
       <div class="desk-map">
         <a class="btn btn-primary" href="{{ route('books.index') }}">Go to My Bookings</a>
-        <img src="{{ asset('/desk-view.jpg') }}" alt="Desk View" width="100%">
+        <img src="{{ asset('/office-desk-view.jpg') }}" alt="Desk View" width="100%">
         <div class="dots"></div>
           <!-- Trigger/Open The Modal -->
           <button id="dot1" class="modal-button" href="#myModal1">A1</button>
