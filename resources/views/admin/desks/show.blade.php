67dfa892f308bee0d0fc14a83517d0af42fb06fb
Carlo Soleta <carlosoleta@student.laverdad.edu.ph>
2022-05-06 07:21:40 +0800

    Desk Management Features

---
+++ resources/views/admin/desks/show.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.sidebar_admin')
+
+@section('content')
+  <section class="body-section">
+    <div class="body-header">
+      <h1 class="content-title">ADMIN HOME PAGE</h1>
+    </div>
+    <div class="body-content">
+      <div class="home">
+        <div class="row">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left">
+                    <h2> Show Desk</h2>
+                </div>
+                <div class="pull-right">
+                    <a class="btn btn-primary" href="{{ route('desks.index') }}"> Back</a>
+                </div>
+            </div>
+        </div>
+       
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Desk:</strong>
+                    {{ $desk->desk_number }}
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+              <div class="form-group">
+                <strong>Desk Map:</strong>
+                <img src="/desks/{{ $desk->desk_map }}" width="500px">
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
+@endsection
