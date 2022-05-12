92d878c6bf0784ce47bfa09984438339467611a4
Carlo Soleta <carlosoleta@student.laverdad.edu.ph>
2022-05-13 07:45:05 +0800

    Some minor changes

--- resources/views/office_map.blade.php
+++ resources/views/office_map.blade.php
@@ -6,11 +6,6 @@
     <h1 class="content-title">Office Map</h1>
     </div>
     <div class="body-content">
-      @if(session('error'))
-      <div class="alert-fail">
-        {{ session('error') }}
-      </div><br>
-      @endif
       <div class="desk-map">
         <a class="btn btn-primary go-to" href="{{ route('books.index') }}">Go to My Bookings</a>
         <img src="{{ asset('/office-view-with-code.png') }}" alt="Desk View" width="100%">
