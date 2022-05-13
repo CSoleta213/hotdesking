599c25461b3753693c73ef9b6727718ddaf077f1
Carlo Soleta <carlosoleta@student.laverdad.edu.ph>
2022-05-13 09:18:28 +0800

    Replace sample overview to actual

--- resources/views/features.blade.php
+++ resources/views/features.blade.php
@@ -18,7 +18,7 @@
             </p>        
           </div>
         </div>
-        <a href="#"><div class="columns image" style="background: #FF4269"> </div></a>
+        <a href="#"><iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%234285F4&ctz=Asia%2FManila&title=Hot%20Desking&src=Y18yNmFsNXV2NnViOGptbGxkbTAybHJraXZlOEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4ucGhpbGlwcGluZXMjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23009688&color=%230B8043" style="border:solid 1px #777" width="475" height="350" frameborder="0" scrolling="no"></iframe></a>
       </div>
 
        <!-- <div class="section-container">
@@ -28,7 +28,7 @@
 
       <div class="section-container">
         <a href="{{ url('/features/desk-map') }}">
-          <div class="columns image"><img src="{{ asset('/office-desk-view.jpg') }}" alt="Reservation Listing" width="100%"></div>
+          <img src="{{ asset('/office-desk-view.jpg') }}" alt="Reservation Listing" width="100%">
         </a>
     
         <div class="columns content">
@@ -55,8 +55,40 @@
             </p>
           </div>
         </div>
-        <a href="{{ url('/features/desk-map') }}">
-          <div class="columns image" id="desk-view"></div>
+        <a href="{{ url('/books') }}" style="text-decoration: none;">
+          <!-- <div class="columns image" id="desk-view"> -->
+          <table class="list-table" style="font-size: 10px">
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Office Name</th>
+              <th>Desk Number</th>
+              <th>Date</th>
+            </tr>
+            @forelse ($data as $key => $value)
+              @if ($value->name === Auth::user()->firstname." ".Auth::user()->lastname)
+              <tr>
+                  <td>{{ ++$i }}</td>
+                  <td>{{ $value->name }}</td>
+                  <td>{{ $value->office_name }}</td>
+                  <td style="text-transform:uppercase">{{ $value->desk_number }}</td>
+                  <td>{{ $value->date }}</td>
+                  <td >
+                    <form action="{{ route('books.destroy',$value->id) }}" method="POST" class="action" style="font-size: 10px">   
+                      <a class="read" href="{{ route('books.show',$value->id) }}" style="font-size: 10px"> Show</a>
+                      <!-- <a class="update" href="{{ route('books.edit',$value->id) }}" style="font-size: 10px">Edit</a>   
+                      @csrf
+                      @method('DELETE')      
+                      <button type="submit" class="destroy" style="font-size: 10px">Delete</button> -->
+                    </form>
+                  </td>
+              </tr>
+              @endif
+              @empty
+              <center><h1 style="color: #551A8B">You haven't book yet!</h1></center>
+            @endforelse
+          </table>
+          <!-- </div> -->
         </a>
       </div>
     
