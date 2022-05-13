3d4049f1ee062893f6ebd61610e76b75cc159a54
Carlo Soleta <carlosoleta@student.laverdad.edu.ph>
2022-05-13 09:21:21 +0800

    Adding message if user has no booking yet.

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -36,27 +36,28 @@
           <th>Date</th>
           <th width="280px">Action</th>
         </tr>
-        @foreach ($data as $key => $value)
-          @if ($value->name === Auth::user()->firstname." ".Auth::user()->lastname)
-          <tr>
-              <td>{{ ++$i }}</td>
-              <td>{{ $value->name }}</td>
-              <td>{{ $value->office_name }}</td>
-              <td style="text-transform:uppercase">{{ $value->desk_number }}</td>
-              <td>{{ $value->date }}</td>
-              <!-- <td>{{ \Str::limit($value->description, 100) }}</td> -->
-              <td >
-                <form action="{{ route('books.destroy',$value->id) }}" method="POST" class="action">   
-                  <a class="read" href="{{ route('books.show',$value->id) }}" > Show</a>
-                  <a class="update" href="{{ route('books.edit',$value->id) }}">Edit</a>   
-                  @csrf
-                  @method('DELETE')      
-                  <button type="submit" class="destroy">Delete</button>
-                </form>
-              </td>
-          </tr>
-          @endif
-        @endforeach
+        @forelse ($data as $key => $value)
+              @if ($value->name === Auth::user()->firstname." ".Auth::user()->lastname)
+              <tr>
+                  <td>{{ ++$i }}</td>
+                  <td>{{ $value->name }}</td>
+                  <td>{{ $value->office_name }}</td>
+                  <td style="text-transform:uppercase">{{ $value->desk_number }}</td>
+                  <td>{{ $value->date }}</td>
+                  <td >
+                    <form action="{{ route('books.destroy',$value->id) }}" method="POST" class="action">   
+                      <a class="read" href="{{ route('books.show',$value->id) }}"> Show</a>
+                      <a class="update" href="{{ route('books.edit',$value->id) }}">Edit</a>   
+                      @csrf
+                      @method('DELETE')      
+                      <button type="submit" class="destroy">Delete</button>
+                    </form>
+                  </td>
+              </tr>
+              @endif
+              @empty
+              <center><h1 style="color: #551A8B">You haven't book yet!</h1></center>
+            @endforelse
       </table>
       <div style="display: flex; flex-direction: row; width: 100px">
         {!! $data->links() !!}
